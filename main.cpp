5f9811e6c0de56ffda931f456b302f29228964ae
RusJJ <90149276+AndroidModLoader@users.noreply.github.com>
2022-02-20 22:22:12 +0300

    Initial

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <mod/amlmod.h>
+
+MYMOD(net.rusjj.gtasa.sanltd, GTA:SA SanLTD Activator, 1.0.0, RusJJ)
+
+struct RwTexture;
+
+void* hGTASA;
+uintptr_t pGTASA;
+uintptr_t sanltd_fonts_db;
+uintptr_t pFontSprite[3]; // 0 - font2, 1 - font1, 2 - primary font (CSprite's)
+RwTexture* (*SetSpriteTexture1)(uintptr_t* sprite, const char* texture);
+RwTexture* (*SetSpriteTexture2)(uintptr_t* sprite, const char* texture, const char* mask);
+uintptr_t (*LoadTextureDB)(const char* dbFile, bool fullLoad, int txdbFormat);
+void (*RegisterTextureDB)(uintptr_t dbPtr);
+
+DECL_HOOKv(AddStandartTexture)
+{
+    AddStandartTexture();
+    if(pFontSprite[0] != 0 && pFontSprite[1] != 0)
+    {
+        SetSpriteTexture2(&pFontSprite[0], "font2_sanltd", "font2m");
+        SetSpriteTexture2(&pFontSprite[1], "font1_sanltd", "font1m");
+    }
+}
+DECL_HOOKv(InitialiseRenderWare)
+{
+    InitialiseRenderWare();
+    sanltd_fonts_db = LoadTextureDB("sanltd_fonts", false, 5);
+    RegisterTextureDB(sanltd_fonts_db);
+}
+
+extern "C" void OnModLoad()
+{
+    pGTASA = aml->GetLib("libGTASA.so");
+    hGTASA = dlopen("libGTASA.so", RTLD_LAZY);
+
+    SET_TO(pFontSprite, aml->GetSym(hGTASA, "_ZN5CFont6SpriteE"));
+    SET_TO(SetSpriteTexture1, aml->GetSym(hGTASA, "_ZN9CSprite2d10SetTextureEPc"));
+    SET_TO(SetSpriteTexture2, aml->GetSym(hGTASA, "_ZN9CSprite2d10SetTextureEPcS0_"));
+    SET_TO(LoadTextureDB, aml->GetSym(hGTASA, "_ZN22TextureDatabaseRuntime4LoadEPKcb21TextureDatabaseFormat"));
+    SET_TO(RegisterTextureDB, aml->GetSym(hGTASA, "_ZN22TextureDatabaseRuntime8RegisterEPS_"));
+
+    aml->Write(pGTASA + 0x2A5C78, (uintptr_t)"SanLTD", 7);
+    aml->Write(pGTASA + 0x5A8904 + 0x5, (uintptr_t)"SANFT", 5);
+    aml->Write(pGTASA + 0x61EC30, (uintptr_t)"SANLTD", 6);
+
+    HOOKPLT(AddStandartTexture, pGTASA + 0x6747CC);
+    HOOKPLT(InitialiseRenderWare, pGTASA + 0x66F2D0);
+}
